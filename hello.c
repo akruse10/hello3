7711d20afd07a453210e02736bc2169515ca3753
akruse10 <59975555+akruse10@users.noreply.github.com>
2020-01-16 13:26:10 -0600

    Update hello.c
    we added sum stuff

---
+++ hello.c
@@ -0,0 +1,13 @@
+/* "Hello World", My first programName: Anna Kruse
+Date: 201x/xx/xx*/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World!\n");
+	
+	printf("My Major is Nothing");
+	
+	return 0;
+	
+	}
